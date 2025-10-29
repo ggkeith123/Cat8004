21dba6c5371405471233737f6234293c970fe729
ggkeith123 <162587915+ggkeith123@users.noreply.github.com>
2025-10-29 09:06:26 +0800

    Update contract.ts

--- lib/contract.ts
+++ lib/contract.ts
@@ -1,153 +1,103 @@
-export const CAT8004_ADDRESS = "0x369404B4Cd2BB70375716767Bd6605FC5285cD40";
+export const CAT8004_ADDRESS = "0x0000000000000000000000000000000000000000" as const;
 
 export const CAT8004_ABI = [
   {
-    "inputs": [],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "uint256",
-        "name": "tokenAmount",
-        "type": "uint256"
-      }
+        internalType: "uint256",
+        name: "tokenAmount",
+        type: "uint256",
+      },
     ],
-    "name": "mint",
-    "outputs": [],
-    "stateMutability": "payable",
-    "type": "function"
+    name: "mint",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
   },
   {
-    "inputs": [],
-    "name": "getMintProgress",
-    "outputs": [
+    inputs: [],
+    name: "getMintProgress",
+    outputs: [
       {
-        "internalType": "uint256",
-        "name": "minted",
-        "type": "uint256"
+        internalType: "uint256",
+        name: "minted",
+        type: "uint256",
       },
       {
-        "internalType": "uint256",
-        "name": "total",
-        "type": "uint256"
+        internalType: "uint256",
+        name: "total",
+        type: "uint256",
       },
       {
-        "internalType": "uint256",
-        "name": "available",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "tokenAmount",
-        "type": "uint256"
-      }
-    ],
-    "name": "calculateCost",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "pure",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "totalMinted",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "MAX_SUPPLY",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "MAX_MINTABLE",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "ETH_PRICE_PER_1000_TOKENS",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": true,
-        "internalType": "address",
-        "name": "minter",
-        "type": "address"
+        internalType: "uint256",
+        name: "available",
+        type: "uint256",
       },
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      },
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "ethPaid",
-        "type": "uint256"
-      },
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "totalMinted",
-        "type": "uint256"
-      }
     ],
-    "name": "TokensMinted",
-    "type": "event"
-  }
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "user",
+        type: "address",
+      },
+    ],
+    name: "getExpectedNFTCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getDistributionCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
 ] as const;
 
-// Pricing constants
-export const ETH_PRICE_PER_1000_TOKENS = "0.0025"; // 0.0025 ETH = $10 (at ~$4000/ETH)
-export const TOKENS_PER_10_USD = 1000;
-export const MIN_MINT_AMOUNT = 1; // Minimum 1 token
-export const MAX_MINT_AMOUNT = 5000; // Maximum 5000 tokens per tx
+export const ETH_PRICE_PER_1000_TOKENS = "0.0025";
+export const MIN_MINT_AMOUNT = 1;
+export const MAX_MINT_AMOUNT = 5000;
+export const TOKENS_PER_NFT = 1000;
+export const MAX_SUPPLY = 1000000;
+export const MAX_MINTABLE = 500000;
